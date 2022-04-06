03f4b682b98feb83acf95e6ad4d94689bf659c56
MadhavDas777 <96810521+MadhavDas777@users.noreply.github.com>
2022-04-06 19:16:46 +0530

    Create Bubble sort.c

---
+++ Bubble sort.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+void main()
+{
+	int a[20], i, j, m, n;
+	printf("enter the limit:");
+	scanf("%d", &n);
+	printf("enter the array elements:");
+	for (i = 0; i < n; i++)
+	{
+		scanf("%d", &a[i]);
+	}
+	for (i = 0; i < n; i++)
+	{
+		for (j = i + 1; j < n; j++)
+		{
+			if (a[i] > a[j])
+			{
+				m = a[i];
+				a[i] = a[j];
+				a[j] = m;
+			}
+		}
+	}
+	printf("elements in ascending order:\n");
+	for (i = 0; i < n; i++)
+		printf("%d\t", a[i]);
+}
